6de4ceeeb3ba7d3bcbe62414e33d239d265b4e5b
james <jwin4740@gmail.com>
2018-09-17 00:25:17 -0400

    GRAB REPOS finally

--- node/grabRepos.js
+++ node/grabRepos.js
@@ -1,6 +1,17 @@
-var request = require('request');
-request('http://www.google.com', function (error, response, body) {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', body); // Print the HTML for the Google homepage.
+var GitHub = require('github-api');
+let username = process.argv[2];
+let oathtoken = process.argv[3];
+console.log(oathtoken);
+var gh = new GitHub({
+    token: oathtoken
+});
+
+var me = gh.getUser(username);
+let repoArr = [];
+me.listRepos({}, function (err, repos) {
+    if (err) throw err;
+    repoArr = repos.map(function (val) {
+        return val.name
+    });
+    console.log(repoArr);
 });
